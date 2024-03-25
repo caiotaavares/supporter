a10d9307a4df3ad2cd56c3fbf2327cf17118bba7
caiotaavares <caio_s.tavares2@hotmail.com>
2024-03-25 17:16:07 -0300

    first commit

---
+++ src/main/java/com/supporter/supporterWeb/SupporterWebApplication.java
@@ -0,0 +1,13 @@
+package com.supporter.supporterWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupporterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupporterWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supporter/supporterWeb/SupporterWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supporter.supporterWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupporterWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
